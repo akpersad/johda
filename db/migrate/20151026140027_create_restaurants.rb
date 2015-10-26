fa931aeebe1a6776a426541784d6f74ed2b1088b
Peter Han <han.peter92@gmail.com>
2015-10-26 17:36:20 -0400

    save

--- db/migrate/20151026140027_create_restaurants.rb
+++ db/migrate/20151026140027_create_restaurants.rb
@@ -1,17 +1,17 @@
 class CreateRestaurants < ActiveRecord::Migration
   def change
-    create_table :restaurants do |t|
-    	t.integer :merchant_id
-    	t.string :name
-    	t.string :address
-    	t.float :lat
-    	t.float :long
-    	t.boolean :is_it_open
-    	t.time :last_or_next_order_time
-    	t.float :delivery_charge
-    	t.string :cuisine
-    	t.integer :phonenumber, :limit => 5
-      t.timestamps null: false
+        create_table :restaurants do |t|
+        t.integer :merchant_id
+        t.string :name
+        t.string :address
+        t.float :lat
+        t.float :long
+        t.boolean :is_it_open
+        t.time :last_or_next_order_time
+        t.float :delivery_charge
+        t.string :cuisine
+        t.integer :phonenumber, :limit => 5
+        t.timestamps null: false
     end
   end
 end
