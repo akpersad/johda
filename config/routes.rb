06e08332048cb4c3392ace6b47e32e9b4300fa16
Andrew Persad <akpersad@gmail.com>
2015-10-27 11:22:03 -0400

    added cuisine, only show open

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   # post 'johda/search'
 
-  get "menus#index"
+  get "menus/index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
