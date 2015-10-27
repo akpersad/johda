58571da909e77ca1ced71f37e3a0d79f9a11f570
Peter Han <han.peter92@gmail.com>
2015-10-27 16:49:37 -0400

    rid of an unecesssary method in getrest

--- app/controllers/johda_controller.rb
+++ app/controllers/johda_controller.rb
@@ -6,6 +6,7 @@ class JohdaController < ApplicationController
 	def main
 		@input = params['address']
 		@results = Getrestaurants.new(@input)
+		# @results.page(1).per(10).padding(3)
 		if @results.name == []
 			flash[:success] = "<b>No results were returned. Please try again.</b>"
 			redirect_to ("/")
