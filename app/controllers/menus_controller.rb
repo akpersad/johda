0de28b53b66973ad952e8d286364acb041dfa725
Peter Han <han.peter92@gmail.com>
2015-11-03 13:29:29 -0500

    save

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -3,7 +3,16 @@ class MenusController < ApplicationController
   def index
     @menu = Menu.new(params.key("").to_i)
     @menu.runner
-    
+
+    @input = session['last_search']
+		@results = Getrestaurants.new(@input)
+
+		@results.merchant_id.each_with_index do |e,i|
+			if e == params.key("")
+				@name = @results.name[i]
+			end
+		end
+
     render "index"
 
   end
