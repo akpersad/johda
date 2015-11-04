2422d5c7f87b859a631bfe21ac6fc8ad043b1b18
hnae6443 <hnae6443@gmail.com>
2015-11-04 15:28:23 -0500

    attempt to patch session id issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  def check_if_logged_in
+    deny_access unless logged_in?
+  end
 end
