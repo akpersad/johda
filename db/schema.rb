259beef148589ed8f24549637e37f585ec17d56e
hnae6443 <hnae6443@gmail.com>
2015-11-05 15:12:39 -0500

    profile update

--- db/schema.rb
+++ db/schema.rb
@@ -61,7 +61,6 @@ ActiveRecord::Schema.define(version: 20151105164248) do
     t.string   "name"
     t.integer  "user_id"
     t.integer  "restaurant_id"
-    t.boolean  "complete"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
   end
@@ -86,10 +85,13 @@ ActiveRecord::Schema.define(version: 20151105164248) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
     t.string   "password_digest"
     t.string   "remember_digest"
+    t.string   "activation_digest"
+    t.boolean  "activated",         default: false
+    t.datetime "activated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
