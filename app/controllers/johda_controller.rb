ac6813a14178d32032a3cb88149b731171d97f3a
Lois <tatislois@icloud.com>
2015-11-09 06:38:47 -0500

    fixed filter bug with favs

--- app/controllers/johda_controller.rb
+++ app/controllers/johda_controller.rb
@@ -104,9 +104,11 @@ class JohdaController < ApplicationController
 
 	def save_favs
 		if !session[:user_id].nil?
+
 			restaurant = Restaurant.find_by_id(params['favorite'].to_i)
 			user = User.find_by_id(session[:user_id])
 			user.favorite_restaurants.create(:restaurant => restaurant)
+			
 		end
 		render nothing: true
 	end
